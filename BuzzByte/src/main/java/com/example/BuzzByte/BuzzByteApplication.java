a71b36099890734aeaa55c364bbe8b7e089487fe
tibumanu <tibu.cristian.sdf@gmail.com>
2024-10-22 20:55:55 +0300

    added Spring boot project
    the **`BuzzByte`** folder/directory represents the project itself.

---
+++ BuzzByte/src/main/java/com/example/BuzzByte/BuzzByteApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuzzByte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzByteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuzzByteApplication.class, args);
+	}
+
+}
---
+++ BuzzByte/src/test/java/com/example/BuzzByte/BuzzByteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuzzByte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuzzByteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

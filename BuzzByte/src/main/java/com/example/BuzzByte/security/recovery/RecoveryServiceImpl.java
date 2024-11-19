7f791fd204e10fc28e043b91719da342cdcf430e
AlisaUrsu <115451781+AlisaUrsu@users.noreply.github.com>
2024-11-19 16:14:53 +0200

    Finished authentication

--- BuzzByte/src/main/java/com/example/BuzzByte/security/recovery/RecoveryServiceImpl.java
+++ BuzzByte/src/main/java/com/example/BuzzByte/security/recovery/RecoveryServiceImpl.java
@@ -46,7 +46,6 @@ public class RecoveryServiceImpl implements RecoveryService {
                 "</body>" +
                 "</html>";
         emailService.sendHtmlEmail(user.getEmail(), "Password reset", htmlContent);
-        System.out.println(user);
     }
 
     @Override
--- BuzzByte/src/main/java/com/example/BuzzByte/security/token/TokenRepository.java
+++ BuzzByte/src/main/java/com/example/BuzzByte/security/token/TokenRepository.java
@@ -1,11 +1,13 @@
 package com.example.BuzzByte.security.token;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
+@Hidden
 public interface TokenRepository extends JpaRepository<UserToken, Long> {
     @Query(value = "select t from UserToken t where t.user.id = :id")
     Optional<UserToken> findByUserId(@Param("id") Long id);

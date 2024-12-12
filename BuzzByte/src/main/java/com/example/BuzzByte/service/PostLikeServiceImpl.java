9ff3a2c46f19338d0ec7f1d8367ca6d470bb98dc
AlisaUrsu <115451781+AlisaUrsu@users.noreply.github.com>
2024-12-12 10:09:47 +0200

    fixed like logic

--- BuzzByte/src/main/java/com/example/BuzzByte/service/PostLikeServiceImpl.java
+++ BuzzByte/src/main/java/com/example/BuzzByte/service/PostLikeServiceImpl.java
@@ -1,12 +1,16 @@
 package com.example.BuzzByte.service;
 
 import com.example.BuzzByte.model.Post;
+import com.example.BuzzByte.model.User;
 import com.example.BuzzByte.model.likes.PostLike;
 import com.example.BuzzByte.repository.PostLikeRepository;
+import com.example.BuzzByte.repository.PostRepository;
+import com.example.BuzzByte.repository.UserRepository;
 import com.example.BuzzByte.utils.validation.GenericValidator;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +21,7 @@ public class PostLikeServiceImpl implements PostLikeService {
 
     private final PostLikeRepository postLikeRepository;
     private final GenericValidator<PostLike> postLikeValidator;
+    private final PostRepository postRepository;
     @Override
     @Transactional
     public List<PostLike> getAllLikes() {
@@ -35,6 +40,7 @@ public class PostLikeServiceImpl implements PostLikeService {
         try{
             Post post = like.getPost();
             post.setLikes(post.getLikes() - 1);
+            postRepository.save(post);
             postLikeRepository.deleteById(likeId);
 
         } catch (Exception e) {
@@ -58,6 +64,8 @@ public class PostLikeServiceImpl implements PostLikeService {
         try {
             Post post = like.getPost();
             post.setLikes(post.getLikes() + 1);
+            System.out.println(post.getLikes());
+            postRepository.save(post);
             return postLikeRepository.save(like);
 
         } catch (Exception e) {
@@ -71,6 +79,5 @@ public class PostLikeServiceImpl implements PostLikeService {
         return postLikeRepository.findById(likeId)
                 .orElseThrow(() -> new EntityNotFoundException("Like not found"));
     }
-
 
 }
